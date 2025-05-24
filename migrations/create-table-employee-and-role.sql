8315501380229843a7a44c7289c058340c67da8e
ilya96s <i.kaltygin@icloud.com>
2025-05-24 16:58:23 +0700

    Слой Data. Запускаем сервер базы данных [#505417 #543780]

--- migrations/create-table-employee-and-role.sql
+++ migrations/create-table-employee-and-role.sql
@@ -1,7 +1,7 @@
 create table if not exists employee
 (
     id          bigint primary key generated always as identity,
-    name        text,
+    name        text not null,
     created_at  timestamptz default current_timestamp,
     updated_at  timestamptz default current_timestamp
 );
@@ -9,7 +9,7 @@ create table if not exists employee
 create table if not exists role
 (
     id          bigint primary key generated always as identity,
-    name        text,
+    name        text not null,
     created_at  timestamptz default current_timestamp,
     updated_at  timestamptz default current_timestamp
 );
