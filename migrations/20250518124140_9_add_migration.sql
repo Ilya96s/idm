4057dd127c29666297fb329312549fb59ebae0a7
ilya96s <i.kaltygin@icloud.com>
2025-05-24 16:59:59 +0700

    Слой Data. Мигрируем [#505418 #543782]

--- migrations/20250518124140_9_add_migration.sql
+++ migrations/20250518124140_9_add_migration.sql
@@ -3,7 +3,7 @@
 create table if not exists employee
 (
     id          bigint primary key generated always as identity,
-    name        text,
+    name        text not null,
     created_at  timestamptz default current_timestamp,
     updated_at  timestamptz default current_timestamp
 );
@@ -11,7 +11,7 @@ create table if not exists employee
 create table if not exists role
 (
     id          bigint primary key generated always as identity,
-    name        text,
+    name        text not null,
     created_at  timestamptz default current_timestamp,
     updated_at  timestamptz default current_timestamp
 );
